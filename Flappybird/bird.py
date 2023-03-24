cb717651673a2f237eed34ccb7c28c4814a21232
ronaldofelp <ronaldoamorim144@gmaail.com>
2023-03-24 07:59:19 -0300

    atualizações

--- Flappybird/bird.py
+++ Flappybird/bird.py
@@ -7,9 +7,9 @@ import random
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png' )))
-IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png' )))
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png' )))
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
 IMAGENS_PASSAROS = [
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png' ))),
     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png' ))),
@@ -41,7 +41,7 @@ class Passaro:
         self.altura = self.y
         self.tempo = 0
         self.contagem_imagem = 0
-        self.imagem = IMGS[0]
+        self.imagem = self.IMGS[0]
 
 
 
@@ -73,3 +73,63 @@ class Passaro:
 
             if self.angulo > - 90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
+
+        
+    def desenhar(self, tela):
+        # definir quaal imagem do passaro usar 
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
+            self.imagem = self.IMGS[1]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+
+
+        # se o passaro tiver caindo | não baater asas
+
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
+        
+        # desenhar imagem
+
+        imagem_rotacionada = pygame.transform.ratate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.angulo))
+        retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
+
+
+class Cano:
+    DISTANCIA = 200
+    VELOCIDADE = 5
+    
+    def __init__(self,x):
+        self.x = x
+        self.altura = 0
+        self.pos_top = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pygame.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE  = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    def definir_altura(self):
+        self.altura = random.randrange(50, 450)
+        self.pos_top = self.altura - self.CANO_TOPO.get_height()
+        self.pos_base =  self.altura + self.DISTANCIA
+
+
